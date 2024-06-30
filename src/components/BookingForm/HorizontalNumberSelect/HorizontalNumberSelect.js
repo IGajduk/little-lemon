6b1bf45fa303ecf4e0b04be0149939599b2fb475
igajduk <goloso4ok@gmail.com>
2024-06-29 20:31:59 -0700

    - time and guest number pickers updated for TAB navigation through the website

--- src/components/BookingForm/HorizontalNumberSelect/HorizontalNumberSelect.js
+++ src/components/BookingForm/HorizontalNumberSelect/HorizontalNumberSelect.js
@@ -10,22 +10,19 @@ const HorizontalNumberSelector = ({
     onBlur,
     disabledOptions
 }) => {
-    const selectionChanges = (e) => {
-        onChange(e);
-    };
     const arrOfNumbers = [...Array(amountOfRadioButtons).keys()];
     const radioButtons = arrOfNumbers.map((num) => {
-    const val = num + 1;
-    const isChecked = Number(selectedValue) === val;
-    const isDisabled = disabledOptions && disabledOptions.includes(val);
-    if (isDisabled && isChecked) {
-        // selectionChanges({target: {value: '', name: fieldName}});
-    }
-    const forAttr = `${fieldName}-${num}`;
+        const val = num + 1;
+        const isChecked = Number(selectedValue) === val;
+        const isDisabled = disabledOptions && disabledOptions.includes(val);
+        const forAttr = `${fieldName}-${num}`;
         return <label className="r-item" key={'radio' + fieldName + num} htmlFor={forAttr}>
-            <input id={forAttr} type="radio" name={fieldName} onBlur={onBlur} checked={isChecked} value={val} onChange={selectionChanges} disabled={isDisabled} />
-            <span className="r-label">{val}</span>
+                <input id={forAttr} type="radio" name={fieldName} onBlur={onBlur} checked={isChecked} value={val} onChange={onChange} disabled={isDisabled} />
+                <span className="r-label">{val}</span>
         </label>
+        // <button className="r-item" type='button' onClick={() => !isDisabled && onChange({target: {value: val, name: fieldName}})}>
+                
+        //    </button>
     });
 
     return (
@@ -34,6 +31,7 @@ const HorizontalNumberSelector = ({
             <ScrollContainer nativeMobileScroll={true} style={{overflow: 'scroll'}}>
                 <fieldset id={fieldName} className="horizontal-number-select">
                     {radioButtons}
+                    <div style={{height: '108px'}}></div>
                 </fieldset>
             </ScrollContainer>
         </>
